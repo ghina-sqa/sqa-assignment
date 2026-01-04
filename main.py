076f22e6ed2cd746998ef33cd70113287667de38
ghina-sqa <burnerforoop@outlook.com>
2026-01-04 10:58:24 +0400

    Minor change to if-else statement

--- main.py
+++ main.py
@@ -12,10 +12,9 @@ def analyze_student_performance(student_scores, threshold):
 
         total_score += score
 
+        # minor if-else change
         if score >= threshold:
             weak_count += 1
-        else:
-            pass
 
     average_score = total_score / len(student_scores)
 
@@ -34,3 +33,4 @@ threshold = 50
 
 result = analyze_student_performance(scores, threshold)
 print("Analysis Result:", result)
+
